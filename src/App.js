105e466bd943367da12cae91a2b1081a6354f5b1
Becca <rebecca_waterson@brown.edu>
2022-12-05 15:20:44 -0500

    trying grid for intro thru iterative design

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@
           <a class="navbar-last" href="#AdditionalProjects">More!</a>
         </div>
 
-        <div className="content">
-          <div className="intro" id="Intro">
-            <div className="left-side-intro">
+        <div className="grid-container">
+          
+            <div className="left-side-intro" id="Intro">
               <h2><b>Drowning Dolphin</b></h2>
               <p>Hello World! I am a student of Brown University and member of the Fall 2022 CSCI 1300 UI/UX course.</p>
               <p>Scroll to explore what I've created this semester!</p>
@@ -29,10 +29,9 @@
               alt="Emoticon of dolphin"></img>
             </div>
             
-          </div>
+          
 
-          <div className="item1" id="Personas">
-            <div className="left-side-item1">
+            <div className="left-side-item1" id="Personas">
               <div className="card">
                 <a href="">
                   <img className="screenshots" src=""></img>
@@ -43,10 +42,8 @@
               <h3><b>Learning about Profiles</b></h3>
               <p><i>Describe Assignment Here</i></p>
             </div>
-          </div>
 
-          <div className="item2" id="ResponsiveRedesign">
-            <div className="left-side-item2">
+            <div className="left-side-item2" id="ResponsiveRedesign">
               <h3><b>Developing a Redesign</b></h3>
               <p><i>Describe Assignment Here</i></p>
             </div>
@@ -61,10 +58,8 @@
                 </a>
               </div>
             </div>
-          </div>
 
-          <div className="item3" id="IterativeDesign">
-            <div className="left-side-item3">
+            <div className="left-side-item3"  id="IterativeDesign">
               <div className="card">
                 <a href="https://drowningdolphin123.github.io/iterativedesign/"
                 target="blank">
@@ -78,47 +73,45 @@
               <h3><b>Design with Evaluation</b></h3>
               <p><i>Describe Assignment Here</i></p>
             </div>
-          </div>
+        </div>
+        <div className="item4">
 
-          <div className="item4" id="AdditionalProjects">
-            <div className="top-item4">
-                <h3><b>Fun with React + HTML + Figma</b></h3>
+        <div className="top-item4" id="AdditionalProjects">
+          <h3><b>Fun with React + HTML + Figma</b></h3>
+          </div>
+          <div className="bottom-item4">
+            <div className="card">
+              <a href="https://drowningdolphin123.github.io/development/"
+              target="blank">
+                <img className="screenshots" 
+                src="https://i.imgur.com/hPXWBCo.png"
+                alt="Screenshot of the website designed by Drowning Dolphin for a Club Penguin food court"></img>
+              </a>
             </div>
-            <div className="bottom-item4">
-              <div className="card">
-                <a href="https://drowningdolphin123.github.io/development/"
-                target="blank">
-                  <img className="screenshots" 
-                  src="https://i.imgur.com/hPXWBCo.png"
-                  alt="Screenshot of the website designed by Drowning Dolphin for a Club Penguin food court"></img>
-                </a>
-              </div>
-              <div className="card">
-                <a href=""
-                target="blank">
-                  <img className="screenshots" src="https://i.imgur.com/vnYsFGh.jpg"
-                  alt="Locations Mythos screenshot"></img>
-                </a>
-              </div>
-              <div className="card">
-                <a href="https://www.majorarcana.net/"
-                target="blank">
-                  <img className="screenshots" src="https://i.imgur.com/5hzflr1.png"
-                  alt="MajorArcana Logo"></img>
-                </a>
-              </div>
-              <div className="card">
-                <a href="https://www.figma.com/proto/zmMDkJBsth7OFl1DPsHjMq/LITR-List-project?scaling=scale-down&page-id=0%3A1&starting-point-node-id=5%3A1545&node-id=5%3A1545"
-                target="blank">
-                  <img className="screenshots" 
-                  src="https://i.imgur.com/HYXNB5t.png"
-                  alt="Screenshot of Figma page for Lists project"></img>
-                </a>
-              </div>
+            <div className="card">
+              <a href=""
+              target="blank">
+                <img className="screenshots" src="https://i.imgur.com/vnYsFGh.jpg"
+                alt="Locations Mythos screenshot"></img>
+              </a>
+            </div>
+            <div className="card">
+              <a href="https://www.majorarcana.net/"
+              target="blank">
+                <img className="screenshots" src="https://i.imgur.com/5hzflr1.png"
+                alt="MajorArcana Logo"></img>
+              </a>
+            </div>
+            <div className="card">
+              <a href="https://www.figma.com/proto/zmMDkJBsth7OFl1DPsHjMq/LITR-List-project?scaling=scale-down&page-id=0%3A1&starting-point-node-id=5%3A1545&node-id=5%3A1545"
+              target="blank">
+                <img className="screenshots" 
+                src="https://i.imgur.com/HYXNB5t.png"
+                alt="Screenshot of Figma page for Lists project"></img>
+              </a>
             </div>
           </div>
         </div>
-
         <div className="footer">
           <p>Made with love in Providence</p>
         </div>
