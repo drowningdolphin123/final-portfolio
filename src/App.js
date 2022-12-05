93ab6b8f3350bf1db8fc3b2d69c1319b00216633
Becca <rebecca_waterson@brown.edu>
2022-12-05 13:56:34 -0500

    initial bare-bones design of site to test reactivity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
   import "./App.css";
   import { useState, useEffect } from "react";
-  import shopData from "./assets/shop-data.json";
-  import ShopItem from "./ShopItem";
-  import SideBar from "./SideBar";
+  
 
   /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
   //shopData.forEach((item) => {
@@ -11,109 +9,123 @@
   /* ############################################################## */
 
   function App() {
-    // TODO: use useState to create a state variable to hold the state of the cart
-    /* add your cart state code here */
-    const [cart, setCart] = useState([])
-    const [total, setTotal] = useState(0)
-
-    const [sortBy, setSortBy] = useState("")
-    const [filterBy, setFilterBy] = useState([])
     
-      let displayedItems = shopData.filter((item) => 
-      {for(let i=0; i <= filterBy.length; i++)
-        {if (item.type === filterBy[i]) 
-          return false;
-        }
-        return true;
-      })
-      
-      displayedItems = [...displayedItems].sort((a, b) => {
-        if (a[sortBy] < b[sortBy]) {
-          return -1;
-        }
-        if (a[sortBy] > b[sortBy]) {
-          return 1;
-        } else {
-          return 0;
-        }
-      })
-
-
-    const addToCart = (name, price) => {
-      if (cart.length === 0) {
-        setTotal(price)
-      } else {
-        const newTotal = total + price
-        setTotal(newTotal)
-      }
-      cart.push(name)
-    }
-
-    const removeFromCart = (name, price) => {
-      if (cart.includes(name)) {
-        const index = cart.indexOf(name)
-        if (index > -1) {
-          cart.splice(index, 1)
-        }
-        if (cart.length !== 0) {
-          setTotal(total - price)
-        }
-        if (cart.length === 0) {
-          setTotal(0)
-        }
-      }
-    }
-
-    
-    const resetCart = () => {
-      setTotal(0)
-      cart.length = 0
-    }
- 
-
     return (
       <div className="App">
-        <div className="grid-container">
-        <div className="item1">
-          <img src="https://media.tenor.com/JWm00dg5KXcAAAAj/club-penguin.gif" alt="rotating penguin gif" height="60px"></img>
-        <h1>Penguin Party Palace</h1>
-        <img src="https://media.tenor.com/MJWiL7RJ0qoAAAAj/penguin-dance.gif" alt="breakdancing penguin gif" height="60px"></img>
+        <div className="top-bar">
+
         </div>
 
-        <div className="item2">
-          <SideBar
-          sortingFunction = {setSortBy}
-          filteringFunction = {setFilterBy}
-          ></SideBar>
-        </div>
-
-        <div className="item3">
-          <div className="items">
-            {displayedItems.map((item, index) => (  
-            <div className="card" key={item.name}>
-              <ShopItem {...item} />
-              <button className="button" onClick={() => addToCart(item.image, item.price)}>Add</button>
-              <button className="button" onClick={() => removeFromCart(item.image, item.price)}>Remove</button>
+        <div className="content">
+          <div className="intro">
+            <div className="left-side-intro">
+              <h2><b>Drowning Dolphin</b></h2>
+              <p>Hello World! I am a student of Brown University and member of the Fall 2022 CSCI 1300 UI/UX course.</p>
+              <p>Scroll to explore what I've created this semester!</p>
+              
             </div>
-          ))}
+            <div className="right-side-intro">
+              <img className="dolphin" 
+              src="https://images.emojiterra.com/google/android-11/512px/1f42c.png"
+              alt="Emoticon of dolphin"></img>
+            </div>
+            
+          </div>
+
+          <div className="item1">
+            <div className="left-side-item1">
+              <div className="card">
+                <a href="">
+                  <img className="screenshots" src=""></img>
+                </a>
+              </div>
+            </div>
+            <div className="right-side-item1">
+              <h3><b>Learning about Profiles</b></h3>
+              <p><i>Describe Assignment Here</i></p>
+            </div>
+          </div>
+
+          <div className="item2">
+            <div className="left-side-item2">
+              <h3><b>Developing a Redesign</b></h3>
+              <p><i>Describe Assignment Here</i></p>
+            </div>
+            <div className="right-side-item2">
+              <div className="card">
+                <a id="redesigned-website"
+                  href="https://drowningdolphin123.github.io/responsive-redesign/responsiveRedesign.html"
+                  target="blank">
+                    <img className="screenshots" 
+                    src="https://i.imgur.com/Cp0T6Gb.png"
+                    alt="Screenshot of the website redesign for the Washoe County Library"></img>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="item3">
+            <div className="left-side-item3">
+              <div className="card">
+                <a href="https://drowningdolphin123.github.io/iterativedesign/"
+                target="blank">
+                  <img className="screenshots" 
+                  src="https://i.imgur.com/HfXG7wr.png"
+                  alt="Screenshot of Figma page for Dr. Treat"></img>
+                </a>
+              </div>
+            </div>
+            <div className="right-side-item3">
+              <h3><b>Design with Evaluation</b></h3>
+              <p><i>Describe Assignment Here</i></p>
+            </div>
+          </div>
+
+          <div className="item4">
+            <div className="top-item4">
+                <h3><b>Fun with React + HTML + Figma</b></h3>
+            </div>
+            <div className="bottom-item4">
+              <div className="card">
+                <a href="https://drowningdolphin123.github.io/development/"
+                target="blank">
+                  <img className="screenshots" 
+                  src="https://i.imgur.com/hPXWBCo.png"
+                  alt="Screenshot of the website designed by Drowning Dolphin for a Club Penguin food court"></img>
+                </a>
+              </div>
+              <div className="card">
+                <a href=""
+                target="blank">
+                  <img className="screenshots" src="https://i.imgur.com/vnYsFGh.jpg"
+                  alt="Locations Mythos screenshot"></img>
+                </a>
+              </div>
+              <div className="card">
+                <a href="https://www.majorarcana.net/"
+                target="blank">
+                  <img className="screenshots" src="https://i.imgur.com/5hzflr1.png"
+                  alt="MajorArcana Logo"></img>
+                </a>
+              </div>
+              <div className="card">
+                <a href="https://www.figma.com/proto/zmMDkJBsth7OFl1DPsHjMq/LITR-List-project?scaling=scale-down&page-id=0%3A1&starting-point-node-id=5%3A1545&node-id=5%3A1545"
+                target="blank">
+                  <img className="screenshots" 
+                  src="https://i.imgur.com/HYXNB5t.png"
+                  alt="Screenshot of Figma page for Lists project"></img>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="item4">
-          <div className="cart">
-          <h2>Cart</h2>
-          ${total}
-          <button className="button" onClick={resetCart}>Empty Cart</button>
-          </div>
+        <div className="footer">
+          <p>Made with love in Providence</p>
         </div>
 
-        <div className="item5">
-
-
-        </div>
-        </div>
       </div>
-    );
+    )
   }
 
   export default App;
