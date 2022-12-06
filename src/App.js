4e61d972d9c868389553c49d77d096ff5a33d6ec
Becca <rebecca_waterson@brown.edu>
2022-12-06 12:54:47 -0500

    oh my god this looks so stupid I love it

--- src/App.js
+++ src/App.js
@@ -19,45 +19,79 @@
           
             <div className="left-side-intro" id="Intro">
               <h2><b>Drowning Dolphin</b></h2>
-              <p>Hello World! I am a student of Brown University and member of the Fall 2022 CSCI 1300 UI/UX course.</p>
-              <p>Scroll to explore what I've created this semester!</p>
-              
+              <div>
+                <p>Hello World! I am a student of Brown University and member of the Fall 2022 CSCI 1300 UI/UX course.</p>
+                <p>Scroll to explore what I've created this semester!</p>
+              </div>
             </div>
             <div className="right-side-intro">
               <img className="dolphin" 
               src="https://images.emojiterra.com/google/android-11/512px/1f42c.png"
               alt="Emoticon of dolphin"></img>
             </div>
-            
-          
 
             <div className="left-side-item1" id="Personas">
               <div className="card">
-                <a href="">
-                  <img className="screenshots" src=""></img>
+                <a href=""
+                target="blank">
+                  <img className="screenshots" 
+                  src="https://i.imgur.com/5yV8LAU.png"></img>
                 </a>
               </div>
             </div>
             <div className="right-side-item1">
-              <h3><b>Learning about Profiles</b></h3>
-              <p>
-                Through observing and interviewing individuals interacting with the laundry machines in Hegeman Hall, I created
-                two personas based on their experiences and illustrated storyboards for the personas.
-              </p>
+              <div className="text">
+                <h3><b>Learning about Profiles</b></h3>
+                <div className="descriptions">
+                  <p>
+                    Through observing and interviewing individuals interacting with the laundry machines in Hegeman Hall, I created
+                    two personas based on their experiences and illustrated storyboards for the personas.
+                  </p>
+                </div>
+              </div>
+              <div className="bubbles">
+                <table>
+                  <tr>
+                    <td></td>
+                    <td><img className="bubble" src="https://i.imgur.com/4pXynPQ.png"></img></td>
+                  </tr>
+                  <tr>
+                    <td><img className="bubble" src="https://i.imgur.com/sIb0gaZ.jpg"></img></td>
+                    <td></td>
+                  </tr>
+                </table>
+              </div>
             </div>
 
             <div className="left-side-item2" id="ResponsiveRedesign">
-              <h3><b>Developing a Redesign</b></h3>
-              <p>
-                In this project, I learned and practiced the workflow of redesigning a simple website.
-              </p>
-              <p>
-                I first identified the Washoe County Library website as a digital space that needed renovation, then analyzed and identified flaws in the existing interface.
-               </p>
-               <p> 
-                From there, I created low-fidelity and high-fidelity prototypes for various screensizes, and then learned how to build a 
-                responsive website based on those prototypes.
-              </p>
+            <div className="bubbles">
+                <table>
+                  <tr>
+                    <td><img className="bubble" src="https://i.imgur.com/HzXbXMd.jpg"></img></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td><img className="bubble" src="https://i.imgur.com/SfCdaAf.jpg"></img></td>
+                  </tr>
+                </table>
+              </div>
+              <div className="text">
+                <h3><b>Developing a Redesign</b></h3>
+                <div className="descriptions">
+                  <p>
+                    In this project, I learned and practiced the workflow of redesigning a simple website.
+                  </p>
+                  <p>
+                    I first identified the Washoe County Library website as a digital space that needed renovation, then analyzed and identified flaws in the existing interface.
+                  </p>
+                  <p> 
+                    From there, I created low-fidelity and high-fidelity prototypes for various screensizes, and then learned how to build a 
+                    responsive website based on those prototypes.
+                  </p>
+                </div>
+              </div>
+            
             </div>
             <div className="right-side-item2">
               <div className="card">
@@ -65,7 +99,7 @@
                   href="https://drowningdolphin123.github.io/responsive-redesign/responsiveRedesign.html"
                   target="blank">
                     <img className="screenshots" 
-                    src="https://i.imgur.com/Cp0T6Gb.png"
+                    src="https://i.imgur.com/GqtZ1KJ.png"
                     alt="Screenshot of the website redesign for the Washoe County Library"></img>
                 </a>
               </div>
@@ -76,14 +110,34 @@
                 <a href="https://drowningdolphin123.github.io/iterativedesign/"
                 target="blank">
                   <img className="screenshots" 
-                  src="https://i.imgur.com/HfXG7wr.png"
+                  src="https://bookface-images.s3.amazonaws.com/small_logos/d32db90d7196c1ce3567ce75271b6314a38a8b22.png"
                   alt="Screenshot of Figma page for Dr. Treat"></img>
                 </a>
               </div>
             </div>
             <div className="right-side-item3">
+              <div className="text">
               <h3><b>Design with Evaluation</b></h3>
-              <p><i>Describe Assignment Here</i></p>
+              <div className="descriptions">
+                <p>Worked through the process of mocking up a solution to a chosen startup's content with three other group members.</p>
+                <p>Learned how to break this design pathway into parts, beginning with sketching ideas for the interfaces, then creating
+                  an interactive, high-fidelity prototype, then conducting user testing on a revised prototype, and finally contacting the
+                  startup.
+                </p>
+              </div>
+              </div>
+              <div className="bubbles">
+                <table>
+                  <tr>
+                    <td></td>
+                    <td><img className="bubble" src="https://i.imgur.com/tlM8YXf.png"></img></td>
+                  </tr>
+                  <tr>
+                    <td><img className="bubble" src="https://i.imgur.com/1IaUvDb.png"></img></td>
+                    <td></td>
+                  </tr>
+                </table>
+              </div>
             </div>
         </div>
         <div className="item4">
@@ -96,14 +150,14 @@
               <a href="https://drowningdolphin123.github.io/development/"
               target="blank">
                 <img className="screenshots" 
-                src="https://i.imgur.com/hPXWBCo.png"
+                src="https://media.tenor.com/MJWiL7RJ0qoAAAAj/penguin-dance.gif"
                 alt="Screenshot of the website designed by Drowning Dolphin for a Club Penguin food court"></img>
               </a>
             </div>
             <div className="card">
               <a href=""
               target="blank">
-                <img className="screenshots" src="https://i.imgur.com/vnYsFGh.jpg"
+                <img className="screenshots" src="https://i.imgur.com/Spo00wU.jpg"
                 alt="Locations Mythos screenshot"></img>
               </a>
             </div>
@@ -118,7 +172,7 @@
               <a href="https://www.figma.com/proto/zmMDkJBsth7OFl1DPsHjMq/LITR-List-project?scaling=scale-down&page-id=0%3A1&starting-point-node-id=5%3A1545&node-id=5%3A1545"
               target="blank">
                 <img className="screenshots" 
-                src="https://i.imgur.com/HYXNB5t.png"
+                src="https://i.imgur.com/6Ge9YBY.png"
                 alt="Screenshot of Figma page for Lists project"></img>
               </a>
             </div>
